9b05d623050de4c0c0524d22d8256031453c5d68
Jose Eustaquio <jose.silverio@estudante.iftm.edu.br>
2024-10-30 20:58:52 -0300

    melhor

--- app/src/main/java/com/br/eustaquio/app_contador_ok/MainActivity.kt
+++ app/src/main/java/com/br/eustaquio/app_contador_ok/MainActivity.kt
@@ -1,7 +1,5 @@
 package com.br.eustaquio.app_contador_ok
 
-
-
 import android.os.Bundle
 import android.widget.Button
 import android.widget.TextView
@@ -9,7 +7,7 @@ import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
 
-    // Variável para armazenar o valor do contador
+    // Variável que armazena o valor do contador
     private var counter = 0
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -21,15 +19,16 @@ class MainActivity : AppCompatActivity() {
         val btnIncrease: Button = findViewById(R.id.btnIncrease)
         val btnDecrease: Button = findViewById(R.id.btnDecrease)
 
-        // Inicialmente desativa o botão "Diminuir"
+        // Inicialmente desativa o botão "Diminuir" pois o valor inicial é 0
         btnDecrease.isEnabled = false
 
         // Ação do botão "Aumentar"
         btnIncrease.setOnClickListener {
+            // Incrementa o contador e atualiza a exibição
             counter++
             tvCounter.text = counter.toString()
 
-            // Habilita o botão "Diminuir" se o valor for maior que 0
+            // Habilita o botão "Diminuir" se o valor do contador for maior que 0
             if (counter > 0) {
                 btnDecrease.isEnabled = true
             }
@@ -38,11 +37,12 @@ class MainActivity : AppCompatActivity() {
         // Ação do botão "Diminuir"
         btnDecrease.setOnClickListener {
             if (counter > 0) {
+                // Decrementa o contador e atualiza a exibição
                 counter--
                 tvCounter.text = counter.toString()
             }
 
-            // Desativa o botão "Diminuir" se o valor for 0
+            // Desativa o botão "Diminuir" se o contador chegar a 0
             if (counter == 0) {
                 btnDecrease.isEnabled = false
             }
